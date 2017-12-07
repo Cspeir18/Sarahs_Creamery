a54137bc79b2eb879621454a39de7cbf59c6d4ea
cspeir <cospe18@hotmail.com>
2017-12-07 10:37:40 -0500

    12/7/17

--- app/src/main/java/com/example/cspeir/sarahscreamery/RewardFragment.java
+++ app/src/main/java/com/example/cspeir/sarahscreamery/RewardFragment.java
@@ -1,19 +1,45 @@
 package com.example.cspeir.sarahscreamery;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 /**
  * Created by cspeir on 12/6/2017.
  */
 
 public class RewardFragment extends Fragment {
+    public final static int QRcodeWidth = 500 ;
+    private static final String IMAGE_DIRECTORY = "/QRcodeDemonuts";
+    Bitmap bitmap ;
+    private ImageView iv;
+    private Button btn;
+    Button generateQr;
     private Reward mReward;
     public RewardFragment() {
 
@@ -41,13 +67,103 @@ public class RewardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_reward, parent, false);
-        TextView rewardNameText, rewarddescriptionText;
+        iv  = ( ImageView ) v.findViewById(R.id.img_result);
+        final TextView rewardNameText, rewarddescriptionText, cautionText;
         rewardNameText = (TextView) v.findViewById(R.id.qr_reward_name);
+        cautionText = (TextView) v.findViewById(R.id.textView);
         rewarddescriptionText = (TextView) v.findViewById(R.id.qr_reward_description);
+        btn = (Button) v.findViewById(R.id.qr_code_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mReward.getDirection().length() == 0){
+                    Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                }else {
+                    try {
+                        bitmap = TextToImageEncode(mReward.getDirection());
+                        iv.setImageBitmap(bitmap);
+                        String path = saveImage(bitmap);
+                        iv.setVisibility(View.VISIBLE);
+                        btn.setVisibility(View.GONE);
+                        cautionText.setVisibility(View.GONE);
+                         //give read write permission
+
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        });
 
         rewardNameText.setText(mReward.getRewardName());
         rewarddescriptionText.setText(mReward.getDescription());
-
         return v;
     }
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+
+        if (!wallpaperDirectory.exists()) {
+            Log.d("dirrrrrr", "" + wallpaperDirectory.mkdirs());
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();   //give read write permission
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(getContext(),
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+
+    }
+    private Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        getResources().getColor(R.color.black):getResources().getColor(R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+
 }
